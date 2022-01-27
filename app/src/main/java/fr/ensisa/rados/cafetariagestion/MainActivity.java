de4f286cd6ae4b771899d4656fb4cebda46604bd
RADOS Alexandre <alexandre.rados@uha.fr>
2022-01-27 21:37:56 +0100

    Ca tourne on add et tout

--- app/src/main/java/fr/ensisa/rados/cafetariagestion/MainActivity.java
+++ app/src/main/java/fr/ensisa/rados/cafetariagestion/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import fr.ensisa.rados.cafetariagestion.database.AppDatabase;
 import fr.ensisa.rados.cafetariagestion.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,11 +28,18 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.productFragment, R.id.productListFragment)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        AppDatabase.createInstance(getApplicationContext());
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return Navigation.findNavController(this, R.id.nav_host_fragment_activity_main).navigateUp() || super.onSupportNavigateUp();
     }
 
 }
--- app/src/main/java/fr/ensisa/rados/cafetariagestion/ui/ProductListFragment.java
+++ app/src/main/java/fr/ensisa/rados/cafetariagestion/ui/ProductListFragment.java
@@ -10,6 +10,38 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import fr.ensisa.rados.cafetariagestion.R;
+import fr.ensisa.rados.cafetariagestion.database.AppDatabase;
+import fr.ensisa.rados.cafetariagestion.database.FeedDatabase;
+import fr.ensisa.rados.cafetariagestion.databinding.ProductItemBinding;
+import fr.ensisa.rados.cafetariagestion.databinding.ProductListFragmentBinding;
+import fr.ensisa.rados.cafetariagestion.model.Product;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
@@ -37,7 +69,7 @@ public class ProductListFragment extends Fragment {
 
 
     @Override
-    public void onCreate(@NonNull Bundle savedInstanceState){
+    public void onCreate(@NonNull Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
     }
@@ -46,12 +78,12 @@ public class ProductListFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.product_list_fragment, container, false);
-        root.findViewById(R.id.add).setOnClickListener((view)->{
+        root.findViewById(R.id.add).setOnClickListener((view) -> {
             NavHostFragment.findNavController(this)
                     .navigate(R.id.action_productListFragment_to_productFragment);
         });
         RecyclerView list = root.findViewById(R.id.list);
-        list.setLayoutManager(new LinearLayoutManager(root.getContext(),RecyclerView.VERTICAL,false));
+        list.setLayoutManager(new LinearLayoutManager(root.getContext(), RecyclerView.VERTICAL, false));
 
         adapter = new ProductListAdapter();
         list.setAdapter(adapter);
@@ -74,13 +106,14 @@ public class ProductListFragment extends Fragment {
 
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        inflater.inflate(R.menu.products_menu,menu);
+        inflater.inflate(R.menu.products_menu, menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.populate: return doPopulate();
+        switch (item.getItemId()) {
+            case R.id.populate:
+                return doPopulate();
         }
         return onContextItemSelected(item);
     }
@@ -99,16 +132,17 @@ public class ProductListFragment extends Fragment {
         private class ProductHolder extends RecyclerView.ViewHolder {
 
             ProductItemBinding binding;
+
             public ProductHolder(ProductItemBinding binding) {
                 super(binding.getRoot());
-                this.binding=binding;
+                this.binding = binding;
             }
         }
 
         @NonNull
         @Override
         public ProductHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            ProductItemBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()),R.layout.product_item,parent,false);
+            ProductItemBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.product_item, parent, false);
             return new ProductHolder(binding);
         }
 
@@ -121,7 +155,7 @@ public class ProductListFragment extends Fragment {
 
         @Override
         public int getItemCount() {
-            return products != null ?products.size() : 0;
+            return products != null ? products.size() : 0;
         }
 
         public void setProducts(List<Product> products) {
